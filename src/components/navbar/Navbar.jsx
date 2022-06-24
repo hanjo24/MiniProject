a5b6a9ed8309171daf9698af06f84317b223cb66
Nike Fernandez <nikegfernandez@gmail.com>
2022-06-25 00:57:08 +0800

    First commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../image/logo.png';
+
+const Menu = () => (
+        <>
+        <p><a href='#home'>Home</a></p>
+        <p><a href='#team'>Meet the Team</a></p>
+        <p><a href='#projects'>Projects</a></p>
+        <p><a href='#contactus'>Contact Us</a></p>
+        </>
+)
+// BEM - Block Element Modifier
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className='devlaza_navbar'>
+      <div className='devlaza_navbar-links'>
+        <div className='devlaza_navbar-links-logo'>
+            <img src={logo} alt="logo" />
+        </div>
+        <div className='devlaza_navbar-links_container'>
+        <Menu />
+        </div>
+      </div>
+      <div className='devlaza_navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+
+      {/* MOBILE NAVBAR */}
+
+      <div className='devlaza_navbar-menu'>
+        {toggleMenu
+        ? <RiCloseLine color="#fff" size={27} onClick={ () => setToggleMenu(false)} />
+        : <RiMenu3Line color="#FFF" size={27} onClick={ () => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='devlaza_navbar-menu_container scale-up-center'>
+            <div className='devlaza_navbar-menu_container-links'>
+            <Menu />
+            <div className='devlaza_navbar-menu_container-links-sign'>
+              <p>Sign in</p>
+              <button type='button'>Sign up</button>
+           </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './header.css';
+import people from '../../image/people.png';
+import atom from '../../image/atom.png';
+
+const Header = () => {
+  return (
+    <div className='devlaza_header section_padding' id='home'>
+      <div className='devlaza_header-content'>
+        <h1 className='gradient_text'>We build delightful digital experiences</h1>
+        <p>Affordable, innovative, timely. With over 15 years of experience in software development and digital marketing, we plan, build and promote new applications so you don’t have to.</p>
+        <div className='devlaza_header-content_input'>
+          <input type="email" placeholder="Your email address" />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='devlaza_header-content_people'>
+          <img src={people} alt="people" />
+          <p>1500 people requested access a visit in last 24hrs</p>
+        </div>
+      </div>
+        <div className='devlaza_header-image'>
+          <img src={atom} alt="robot" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
