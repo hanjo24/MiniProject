01553c0e671458e179d635cd28224f02837b3f4c
PaulMaverick <paulpastor122297@gmail.com>
2022-06-25 09:37:39 +0800

    added the project section

---
+++ src/containers/projects/Projects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './project.css'
+import { Navbar } from '../../components';
+
+export default function Projects() {
+    return (
+        <div>
+            <Navbar />
+
+            <main>
+                <div>
+                     <h1>Our team has worked on 300+ projects</h1>
+                     <h3>Check out our works below</h3>
+                </div>
+                <div>
+                    
+                </div>
+            </main>
+        </div>
+    )
+}
